14fe88caa24f4a952fef9df0c618d733cf31810a
Marco Contreras <m.contreras28@ufromail.cl>
2024-09-08 21:26:16 -0300

    commit con el caso de numero resuelto

--- src/main/java/palindromos.java
+++ src/main/java/palindromos.java
@@ -1,29 +1,44 @@
 public class palindromos {
     public static void main(String[] args) {
-       String caso1 ="200";
-       String caso2 ="";
-       String caso3 ="aaabccbaaa";
-       String caso4 ="ahabccbaaa";
-       String caso5 ="La tele letal";
-        System.out.println("200 es palindromo?" + esPalindromo(caso1));
+        String caso1 = "200";
+        String caso2 = "";
+        String caso3 = "aaabccbaaa";
+        String caso4 = "ahabccbaaa";
+        String caso5 = "La tele letal";
+        esPalindromo(caso1);
         System.out.println(" '' es palindromo?" + esPalindromo(caso2));
         System.out.println("aaabccbaaa es palindromo?" + esPalindromo(caso3));
         System.out.println(" ahabccbaaa es palindromo?" + esPalindromo(caso4));
         System.out.println(" La tele letal es palindromo?" + esPalindromo(caso5));
 
 
-
     }
-    public static boolean esPalindromo(String palindromo) {
+
+    public static boolean esPalindromo(String texto) {
+        try {
+            if (contieneNumeros(texto)) {
+                System.out.println("Error: Los números no están permitidos");
+                return false;
+            }
+            String textoInvertido = invertirTexto(texto);
+            return texto.equals(textoInvertido);
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public static String invertirTexto(String palindromo) {
         palindromo = palindromo.toLowerCase();
         String palindromoAlReves = "";
         for (int i = palindromo.length() - 1; i >= 0; i--) {
             palindromoAlReves += palindromo.charAt(i);
-            if (palindromo.equals(palindromoAlReves) ){
-                return true;
-            }
-
         }
-        return false;
+        return palindromoAlReves;
     }
+    public static boolean contieneNumeros(String texto) {
+        return texto.matches(".*\\d.*");
+    }
+
 }
